0549c26a2e22e66dc3f97901450e3f5dc1e67c38
sandalikawmudi <sandalikawmudi2003@gmail.com>
2026-01-01 16:54:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Estate-Agent-Client-side-Web-Application-/',   S
+  base: '/Estate-Agent-Client-side-Web-Application-/',
 })
